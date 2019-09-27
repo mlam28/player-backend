04977e94fb97dcb50950b2db0bf454c5742ccb53
Michelle Lam <shelly2.lam@gmail.com>
2019-09-27 17:44:16 -0400

    can add songs

--- app/controllers/song_playlists_controller.rb
+++ app/controllers/song_playlists_controller.rb
@@ -1,2 +1,17 @@
 class SongPlaylistsController < ApplicationController
+
+    def index
+        render json: SongPlaylist.all
+    end
+
+    def create
+        song = SongPlaylist.create(song_params)
+        render json: {playlist_id: song.playlist.id, song: song}
+    end
+
+    private
+
+    def song_params
+        params.require(:song_playlist).permit(:name, :uri, :time, :artist_uri, :playlist_id, :artist)
+    end
 end
