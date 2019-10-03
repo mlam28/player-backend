5ca830005ca02f52303de27433b355e62ee32cee
mlam28 <michellelam@Michelles-MacBook-Pro.local>
2019-10-03 09:52:17 -0400

    fixed adding user

--- app/controllers/user_playlists_controller.rb
+++ app/controllers/user_playlists_controller.rb
@@ -4,14 +4,15 @@ class UserPlaylistsController < ApplicationController
 
         other_user = User.find_by(spotify_uri: up_params[:add_uri])
 
-        if other_user == nil 
+        if other_user.nil?
+
             render json: {message: 'user must first create an account with app'}
         else 
         user_playlist = UserPlaylist.find_by(user_id: other_user.id, playlist_id: up_params[:playlist_id])
             if user_playlist.nil?
-
+                byebug
             user_playlist = UserPlaylist.create(user_id: other_user.id, playlist_id: up_params[:playlist_id])
-            render json: {message: 'user added to this playlist'}
+            render json: {user: {name: other_user.display_name, spotify_uri: other_user.spotify_uri}, message: 'user added to this playlist'}
             else
 
             render json: {message: 'user is already added to this playlist'}
